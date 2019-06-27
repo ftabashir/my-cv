b049815320e4e011445b0031836fe110fca74d5c
Farzad Tabashir <ftabashir@gmail.com>
2019-06-27 18:34:27 +0430

    add multipe boxes

--- src/app/App.kt
+++ src/app/App.kt
@@ -5,25 +5,61 @@ import react.dom.*
 import logo.*
 import ticker.*
 
-val box = Box(
-    "Education",
-    arrayOf(
-        BoxItem(
-            "M.S.  Artificial Intelligence",
-            "Shiraz University, Shiraz, Iran.",
-            "2015-2018"
-        ),
-        BoxItem(
-            "B.S. Computer Science & Engineering ",
-            "Shiraz University, Shiraz, Iran.",
-            "2010-2015"
+val boxes = arrayOf(
+    Box(
+        "Education",
+        arrayOf(
+            BoxItem(
+                "M.S.  Artificial Intelligence",
+                "Shiraz University, Shiraz, Iran.",
+                "2015-2018"
+            ),
+            BoxItem(
+                "B.S. Computer Science & Engineering ",
+                "Shiraz University, Shiraz, Iran.",
+                "2010-2015"
+            )
+        )
+    ),
+    Box(
+        "Experience",
+        arrayOf(
+            BoxItem(
+                "Otaghak, Tehran, Iran — Android  Developer",
+                "We develop high quality softwares at Otaghak to facilitate finding and booking perfect rental places.",
+                "January 2019 - Now"
+            ),
+            BoxItem(
+                "Communere Ltd, London, UK (Remote) — Full-stack Javascript Developer",
+                "Communere is a bespoke software development company. We deliver full-stack web applications with Cutting-edge technologies to our customers.",
+                "October 2018 - December 2018"
+            ),
+            BoxItem(
+                "Dokan (IR-FMCG), Tehran, Iran — Software Developer",
+                "Developing mobile (Android) & web (Angular, ASP.NET) applications which provides online supermarket and groceries shop.",
+                "June 2017 - September 2018"
+            ),
+            BoxItem(
+                "Rando,  Shiraz, Iran — Android Developer",
+                "Developing android mobile apps for Rando which was an online taxi platform (similar to Uber, Snapp and Tap30)",
+                "June 2016 - June 2017"
+            ),
+            BoxItem(
+                "Raman LTG, Shiraz, Iran — Android Developer",
+                "Developing Augmented Reality (AR) applications for android devices.",
+                "March 2015 - September 2015"
+            )
+
         )
     )
 )
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        chrono_box(box, topEmptySpace=100)
+        boxes.mapIndexed{ index, box ->
+            val topEmptySpace = if(index==0) 100 else 0
+            chrono_box(box, topEmptySpace)
+        }
         div("App-header") {
             
             h2 {
